fb332fdf8c1cff6363454f352c132b1ebb0cd4a2
MengseuThoeng <mengseu2004@gmail.com>
2025-02-03 14:58:10 +0700

    remove base role

--- src/main/java/kh/edu/cstad/gateway/security/SecurityConfig.java
+++ src/main/java/kh/edu/cstad/gateway/security/SecurityConfig.java
@@ -54,10 +54,10 @@ public class SecurityConfig {
 
         http
                 .oauth2Login(oAuth2LoginSpec -> {
-                    oAuth2LoginSpec.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/success"));
+                    oAuth2LoginSpec.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/"));
                     oAuth2LoginSpec.authenticationFailureHandler(new RedirectServerAuthenticationFailureHandler("/401"));
                     oAuth2LoginSpec.authorizationRequestResolver(pkceResolver(repository));
-                    oAuth2LoginSpec.authenticationSuccessHandler(roleBasedAuthenticationSuccessHandler);
+//                    oAuth2LoginSpec.authenticationSuccessHandler(roleBasedAuthenticationSuccessHandler);
 
                 })
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
